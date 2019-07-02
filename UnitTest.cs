7189de2f82cdad8417b71a4b7cb5007b798812a1
USER <USER@KYNQHSE62812>
2019-07-02 09:27:53 +0700

    commit

--- UnitTest.cs
+++ UnitTest.cs
@@ -1,63 +1,157 @@
 ﻿using System;
 using System.Collections.ObjectModel;
-using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Winium;
-
+using System.Threading;
+using System.Runtime.InteropServices;
+using OpenQA.Selenium.Interactions;
 
 namespace UnitTestProject1
 {
     [TestClass]
     public class UnitTest : LoginTest
     {
-        //private const string ClassName = "WindowsForms10.EDIT.app.0.205b375_r9_ad1";
-        private const string ClassName = "WindowsForms10.Window.8.app.0.205b375_r9_ad1";
-       
-        public IWebElement btnLogin;
-
-               
+        ReadOnlyCollection<IWebElement> textBox = driver.FindElementsById("textBox");
+        Actions action = new Actions(driver);
         [TestMethod]
-        public void UpdateProduct()
+        public void VerifyUserCancelOrder()
         {
-            //ReadOnlyCollection<IWebElement> UpdateBtn = driver.FindElementsById("btnConfig");
-            driver.FindElementById("btnConfig").Click();
-            //driver.FindElementByName("").Click();
-            Thread.Sleep(2000);
-            driver.FindElementById("btnUpdate").Click();
-            Thread.Sleep(2000);
-            driver.FindElementById("btnUpdateProduct").Click();
-            //tututrur
+            action.MoveToElement(driver.FindElementById("ptbLogo"), 280, 120).Click().Perform();
+            driver.FindElementById("flexButton1").Click();
+            action.MoveToElement(driver.FindElementById("SaleScreen3"), 95, 208).Click().Perform();
+            action.MoveToElement(driver.FindElementById("SaleScreen3"), 1745, 123).Click().Perform();
+            driver.FindElementById("btnCancel").Click();
+            driver.FindElementById("lblCancel").Click();
         }
 
         [TestMethod]
-        public void UpdateCategory()
+        public void VerifyChooseSizeSuccessful()
         {
-            //ReadOnlyCollection<IWebElement> UpdateBtn = driver.FindElementsById("btnConfig");
-            driver.FindElementById("btnConfig").Click();
-            //driver.FindElementByName("").Click();
-            Thread.Sleep(2000);
-            driver.FindElementById("btnUpdate").Click();
-            Thread.Sleep(2000);
-            driver.FindElementById("btnUpdateCategory").Click();
+            action.MoveToElement(driver.FindElementById("ptbLogo"), 280, 120).Click().Perform();
+            driver.FindElementById("flexButton1").Click();
+            action.MoveToElement(driver.FindElementById("SaleScreen3"), 95, 208).Click().Perform();
+            action.MoveToElement(driver.FindElementById("SaleScreen3"), 1745, 123).Click().Perform();
+            driver.FindElementById("btnAttribute").Click();
+            action.MoveToElement(driver.FindElementById("SaleScreen3"), 1382, 347).Click().Perform();
+            driver.FindElementById("btnClose").Click();
+        }
 
+        [TestMethod]
+        public void VerifyChooseQuantitySuccessful()
+        {
+            action.MoveToElement(driver.FindElementById("ptbLogo"), 280, 120).Click().Perform();
+            driver.FindElementById("flexButton1").Click();
+            action.MoveToElement(driver.FindElementById("SaleScreen3"), 95, 208).Click().Perform();
+            action.MoveToElement(driver.FindElementById("SaleScreen3"), 1745, 123).Click().Perform();
+            driver.FindElementById("btn0").Click();
+            driver.FindElementById("btnClose").Click();
+        }
+
+        [TestMethod]
+        public void VerifyChooseQuantitySuccessfulByPlusOrMinus()
+        {
+            action.MoveToElement(driver.FindElementById("ptbLogo"), 280, 120).Click().Perform();
+            driver.FindElementById("flexButton1").Click();
+            action.MoveToElement(driver.FindElementById("SaleScreen3"), 95, 208).DoubleClick().Perform();
+            action.MoveToElement(driver.FindElementById("SaleScreen3"), 37, 210).Click().Perform();
+        }
+
+        [TestMethod]
+        public void VerifyChooseQuantityOrderTheDrinkSizeL()
+        {
+            action.MoveToElement(driver.FindElementById("ptbLogo"), 280, 120).Click().Perform();
+            driver.FindElementById("flexButton1").Click();
+            action.MoveToElement(driver.FindElementById("SaleScreen3"), 95, 208).Click().Perform();
+            action.MoveToElement(driver.FindElementById("SaleScreen3"), 1745, 123).Click().Perform();
+            driver.FindElementById("btnAttribute").Click();
+            action.MoveToElement(driver.FindElementById("SaleScreen3"), 1382, 347).Click().Perform();
+            driver.FindElementById("btnClone").Click();
+            action.MoveToElement(driver.FindElementById("SaleScreen3"), 95, 208).DoubleClick().Perform();
+            action.MoveToElement(driver.FindElementById("SaleScreen3"), 37, 210).Click().Perform();
+        }
+
+        [TestMethod]
+        public void VerifyChooseProperties()
+        {
+            action.MoveToElement(driver.FindElementById("ptbLogo"), 280, 120).Click().Perform();
+            driver.FindElementById("flexButton1").Click();
+            action.MoveToElement(driver.FindElementById("SaleScreen3"), 95, 208).Click().Perform();
+            action.MoveToElement(driver.FindElementById("SaleScreen3"), 1745, 123).Click().Perform();
+            driver.FindElementById("btnAttribute").Click();
+            action.MoveToElement(driver.FindElementById("SaleScreen3"), 1382, 347).Click().Perform();
+            driver.FindElementById("btnExtra1").Click();
+            action.MoveToElement(driver.FindElementById("SaleScreen3"), 1171, 344).Click().Perform();
+            action.MoveToElement(driver.FindElementById("SaleScreen3"), 1166, 485).DoubleClick().Perform();
+            driver.FindElementById("btnClose").Click();
+        }
+
+        [TestMethod]
+        public void VerifyPayment()
+        {
+            action.MoveToElement(driver.FindElementById("ptbLogo"), 280, 120).Click().Perform();
+            driver.FindElementById("flexButton1").Click();
+            action.MoveToElement(driver.FindElementById("SaleScreen3"), 95, 208).DoubleClick().Perform();
+            driver.FindElementById("lblPayment").Click();
+            driver.FindElementById("btnCustomerInfo").Click();
+            action.MoveToElement(driver.FindElementById("SaleScreen3"), 577, 390).Click().SendKeys("0009388109").Perform();
+            driver.FindElementById("btnCheck").Click();
+            driver.FindElementById("btnPromotionInfo").Click();
+            driver.FindElementById("btnStore2").Click();
+            driver.FindElementById("btnPaymentInfo").Click();
+            driver.FindElementById("btnAddPayment").Click();
+            driver.FindElementById("btnTab1").Click();
+            driver.FindElementById("btn100").Click();
+            driver.FindElementById("btnAddPayment").Click();
+            driver.FindElementById("btnFinish").Click();
+        }
+        [TestMethod]
+        public void VerifyPaymentDeletePromotion()
+        {
+            action.MoveToElement(driver.FindElementById("ptbLogo"), 280, 120).Click().Perform();
+            driver.FindElementById("flexButton1").Click();
+            action.MoveToElement(driver.FindElementById("SaleScreen3"), 95, 208).DoubleClick().Perform();
+            driver.FindElementById("lblPayment").Click();
+            driver.FindElementById("btnCustomerInfo").Click();
+            action.MoveToElement(driver.FindElementById("SaleScreen3"), 577, 390).Click().SendKeys("0009388109").Perform();
+            driver.FindElementById("btnCheck").Click();
+            driver.FindElementById("btnPromotionInfo").Click();
+            driver.FindElementById("btnStore2").Click();
+            driver.FindElementById("btnPaymentInfo").Click();
+            driver.FindElementById("btnAddPayment").Click();
+            driver.FindElementById("btnTab1").Click();
+            driver.FindElementById("btn100").Click();
+            driver.FindElementById("btnAddPayment").Click();
+            driver.FindElementById("btnPromotionInfo").Click();
+            driver.FindElementById("lblCancel").Click();
+            driver.FindElementById("btnPaymentInfo").Click();
+            driver.FindElementById("btnAddPayment").Click();
+
+        }
+        [TestMethod]
+        public void VerifyCheckPayment()
+        {
+            action.MoveToElement(driver.FindElementById("ptbLogo"), 280, 120).Click().Perform();
+            driver.FindElementById("flexButton1").Click();
+            action.MoveToElement(driver.FindElementById("SaleScreen3"), 95, 208).DoubleClick().Perform();
+            driver.FindElementById("lblPayment").Click();
+            driver.FindElementById("btn100").Click();
+            driver.FindElementById("btnAddPayment").Click();
+            driver.FindElementById("btnFinish").Click();
+            action.MoveToElement(driver.FindElementById("ptbLogo"), 1675, 1032).Click().Perform();
+            action.MoveToElement(driver.FindElementById("ptbLogo"), 397, 96).Click().Perform();
+            action.MoveToElement(driver.FindElementById("ptbLogo"), 879, 615).Click().Perform();
         }
 
 
-
-        
-        [TestMethod]
-      
         [ClassInitialize]
         public static void loadForm(TestContext context)
         {
             LoadApp(context);
-            IWebElement btnConfig = driver.FindElementById("btnConfig");
- 
             //IWebElement txtUsername = driver.FindElementById("txtUsername");
             //IWebElement txtPassword = driver.FindElementById("txtPassword");
-            //Assert.IsNotNull(btnConfig);
+            //Assert.IsNotNull(txtUsername);
         }
+
     }
 }
